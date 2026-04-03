9069506cfb658383e9459705a6bb85507f18d0d4
Moamen <alkatibmoamen@gmail.com>
2026-04-03 16:56:03 +0200

    update logging

--- src/main/java/de/moamen/database/BookToDatabaseConnectorImpl.java
+++ src/main/java/de/moamen/database/BookToDatabaseConnectorImpl.java
@@ -1,30 +1,47 @@
 package de.moamen.database;
 
 import de.moamen.model.Book;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookToDatabaseConnectorImpl implements BookToDatabaseConnector {
+    private static final Logger logger=LoggerFactory.getLogger(BookToDatabaseConnectorImpl.class);
+    private final DataSource dataSource=new DataSourceImpl();
     @Override
     public void saveBook(Book book) {
-        DataSource dataSource = new DataSourceImpl();
-        if (!dataSource.open()) {
-            System.out.println("Error by opening the datasource");
+        logger.debug("Attempting to save book {}",book);
+        try {
+            if (!dataSource.open()) {
+                logger.error("Error opening datasource");
+                return;
+            }
+            if (dataSource.findBook(book.getIsbn()) == null) {
+                dataSource.insertBook(book);
+                logger.info("book inserted successfully: {}",book);
+            }else {
+                logger.warn("Book already exists in database: {}", book);
+            }
+        }finally {
+            dataSource.close();
         }
-        if (dataSource.findBook(book.getIsbn()) == null) {
-            dataSource.insertBook(book);
-        }
-        dataSource.close();
     }
 
     @Override
     public List<Book> loadBook() {
-        DataSource dataSource = new DataSourceImpl();
-        if (!dataSource.open()) {
-
+        logger.debug("Attempting to load books from database");
+        try {
+            if (!dataSource.open()) {
+                logger.error("Error opening datasource");
+                return new ArrayList<>();
+            }
+            List<Book> bookList = dataSource.bookQuery();
+            logger.info("Loaded {} books from database", bookList.size());
+            return bookList;
+        }finally {
+            dataSource.close();
         }
-        List<Book> bookList = dataSource.bookQuery();
-        dataSource.close();
-        return bookList;
     }
 }
--- src/main/java/de/moamen/service/Library.java
+++ src/main/java/de/moamen/service/Library.java
@@ -3,6 +3,8 @@ package de.moamen.service;
 import de.moamen.database.BookToDatabaseConnector;
 import de.moamen.database.BookToDatabaseConnectorImpl;
 import de.moamen.model.Book;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,26 +12,30 @@ import java.util.List;
 public class Library {
     private List<Book> bookList;
     private static final BookToDatabaseConnector bookToDatabaseConnector=new BookToDatabaseConnectorImpl();
-
+    private static final Logger logger= LoggerFactory.getLogger(Library.class);
 
     public Library(){
         this.bookList=new ArrayList<>();
     }
 
     public void addBook(Book book){
+        logger.debug("Attempting to add book {}", book);
         if (!checkIfExist(book.getIsbn())){
             bookList.add(book);
+            logger.info("book added successfully: {}",book);
         }else {
-            System.out.println("Book is already in the library");
+            logger.warn("Book is already in the library: {}", book);
         }
 
     }
     public void deleteBook(int isbn){
+        logger.debug("Attempting to delete book with isbn {}",isbn);
         Book book=findBook(isbn);
         if (book!=null){
             bookList.remove(book);
+            logger.info("book deleted successfully: {}",book);
         }else{
-            System.out.println("there is no book with the given isbn");
+            logger.warn("Cannot delete book. No book found with isbn {}", isbn);
         }
     }
 
@@ -37,13 +43,18 @@ public class Library {
         return bookList.stream().anyMatch(book -> isbn==book.getIsbn());
     }
     public Book findBook(int isbn){
+        logger.debug("Checking if book with isbn {} exists", isbn);
         for (Book book:bookList){
-            if (book.getIsbn()==isbn)
+            if (book.getIsbn()==isbn){
+                logger.info("book with the isbn:{} found",isbn);
                 return book;
+            }
         }
+        logger.warn("no book with the isbn {} was found",isbn);
         return null;
     }
     public Book findBook(String title) {
+        logger.debug("Searching for book with title {}", title);
        return bookList.stream().filter(book -> book.getTitle()!=null && title.equalsIgnoreCase(book.getTitle())).findFirst().orElse(null);
     }
 
@@ -62,14 +73,18 @@ public class Library {
     }
 
     public void saveInDatabase(){
+        logger.debug("Saving {} books to database", bookList.size());
         for (Book b :
                 bookList) {
             bookToDatabaseConnector.saveBook(b);
         }
+        logger.info("Successfully saved {} books to database", bookList.size());
     }
 
     public void loadFromDatabase(){
+        logger.debug("Loading books from database");
         bookList=bookToDatabaseConnector.loadBook();
+        logger.info("Loaded {} books from database", bookList.size());
     }
 
     public List<Book> getBookList() {
